c81acc5448857127294492844b0958ab3840e6fe
Tim Whitbeck <twhitbeck@gmail.com>
2021-07-06 11:03:55 -0400

    add some tooling
    prettier, tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,26 +1,13 @@
-import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
-import { useRouteData } from "remix";
-
-import stylesUrl from "../styles/index.css";
+import type { MetaFunction } from "remix";
 
 export let meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
-    description: "Welcome to remix!"
+    description: "Welcome to remix!",
   };
 };
 
-export let links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
-};
-
-export let loader: LoaderFunction = async () => {
-  return { message: "this is awesome 😎" };
-};
-
 export default function Index() {
-  let data = useRouteData();
-
   return (
     <div style={{ textAlign: "center", padding: 20 }}>
       <h2>Welcome to Remix!</h2>
@@ -28,7 +15,6 @@ export default function Index() {
         <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
         started.
       </p>
-      <p>Message from the loader: {data.message}</p>
     </div>
   );
 }
