3940f4908f252e6b5a21710a01e44a37cbaed17f
Tim Whitbeck <twhitbeck@gmail.com>
2021-07-14 22:10:25 -0400

    add nav

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
-import type { LinksFunction, LoaderFunction } from "remix";
-import { Meta, Links, Scripts, useRouteData, LiveReload } from "remix";
-import { Outlet } from "react-router-dom";
+import type { LinksFunction } from "remix";
+import { Meta, Links, Scripts, LiveReload } from "remix";
+import { Outlet, Link } from "react-router-dom";
 
 import tailwindUrl from "./styles/tailwind.css";
 
@@ -8,16 +8,11 @@ export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindUrl }];
 };
 
-export let loader: LoaderFunction = async () => {
-  return { date: new Date() };
-};
-
 function Document({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
         <Meta />
         <Links />
       </head>
@@ -31,14 +26,52 @@ function Document({ children }: { children: React.ReactNode }) {
   );
 }
 
+function MaxWidthWrapper({ children }: { children: React.ReactNode }) {
+  return <div className="max-w-7xl mx-auto px-4 sm:px-6">{children}</div>;
+}
+
+function Header() {
+  return (
+    <div className="bg-blue-100 border-b border-blue-200">
+      <MaxWidthWrapper>
+        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+          <div className="flex">
+            <Link
+              className="text-green-800 text-3xl font-bold hover:text-green-900"
+              to="/"
+            >
+              twhitbeck
+            </Link>
+          </div>
+
+          <nav className="flex space-x-10">
+            {[
+              ["/", "Home"],
+              ["/resume", "Resume"],
+              ["/about", "About"],
+            ].map(([to, label]) => (
+              <Link
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+                to={to}
+              >
+                {label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+      </MaxWidthWrapper>
+    </div>
+  );
+}
+
 export default function App() {
-  let data = useRouteData();
   return (
     <Document>
-      <Outlet />
-      <footer>
-        <p>This page was rendered at {data.date.toLocaleString()}</p>
-      </footer>
+      <Header />
+
+      <MaxWidthWrapper>
+        <Outlet />
+      </MaxWidthWrapper>
     </Document>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,19 +2,15 @@ import type { MetaFunction } from "remix";
 
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!",
+    title: "twhitbeck | Home",
+    description: "The homepage of twhitbeck.com",
   };
 };
 
 export default function Index() {
   return (
-    <div style={{ textAlign: "center", padding: 20 }}>
-      <h2>Welcome to Remix!</h2>
-      <p>
-        <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
-        started.
-      </p>
+    <div className="pt-8">
+      <h1>Home</h1>
     </div>
   );
 }
