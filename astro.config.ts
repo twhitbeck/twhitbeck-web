80a3691f37209cbd92b158dcc6af85e998725eaa
Tim Whitbeck <tim.whitbeck@stord.com>
2024-08-05 09:39:35 -0400

    rm cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -6,8 +6,6 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
@@ -33,6 +31,4 @@ export default defineConfig({
     }
   },
   scopedStyleStrategy: "where",
-  output: "server",
-  adapter: cloudflare()
 });
